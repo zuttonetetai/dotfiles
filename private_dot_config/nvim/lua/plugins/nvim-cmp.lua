9b74f65dd4d1e2b7498a0479f2ac3a653c884016
zuttonetetai <test@example.com>
2025-07-28 23:28:01 +0900

    update neovim config

--- private_dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,105 +1,105 @@
 -- 自動補完プラグイン nvim-cmp
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            -- LSPの補完ソース
-            "hrsh7th/cmp-nvim-lsp",
-            -- スニペットエンジン (例: luasnip)
-            "L3MON4D3/LuaSnip",
-            -- スニペット補完ソース
-            "saadparwaiz1/cmp_luasnip",
-            -- バッファ内の単語の補完ソース
-            "hrsh7th/cmp-buffer",
-            -- ファイルパスの補完ソース
-            "hrsh7th/cmp-path",
-            -- vimコマンドラインの補完
-            "hrsh7th/cmp-cmdline",
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			-- LSPの補完ソース
+			"hrsh7th/cmp-nvim-lsp",
+			-- スニペットエンジン (例: luasnip)
+			"L3MON4D3/LuaSnip",
+			-- スニペット補完ソース
+			"saadparwaiz1/cmp_luasnip",
+			-- バッファ内の単語の補完ソース
+			"hrsh7th/cmp-buffer",
+			-- ファイルパスの補完ソース
+			"hrsh7th/cmp-path",
+			-- vimコマンドラインの補完
+			"hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
-            cmp.setup({
-                performance = {
-                    debounce = 500,
-                    throttle = 50,
-                },
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
-                sources = cmp.config.sources({
-                    { name = "copilot" },
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                    { name = 'render-markdown' },
-                }),
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-Space>"] = cmp.mapping.complete(),            -- 補完ウィンドウ表示
-                    ["<C-e>"] = cmp.mapping.abort(),                   -- 補完中止
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- 補完確定
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    -- <C-p> と <C-n> のマッピングをオーバーライド
-                    ["<C-p>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        else
-                            -- 補完メニュー非表示時はグローバルマッピング (<Up>) を使用
-                            fallback()
-                        end
-                    end, { "i" }), -- インサートモードのみで適用
-                    ["<C-n>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        else
-                            -- 補完メニュー非表示時はグローバルマッピング (<Down>) を使用
-                            fallback()
-                        end
-                    end, { "i" }), -- インサートモードのみで適用
-                }),
-            })
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = "buffer" },
-                },
-            })
+			cmp.setup({
+				performance = {
+					debounce = 2000,
+					throttle = 50,
+				},
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				sources = cmp.config.sources({
+					{ name = "copilot" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "render-markdown" },
+				}),
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(), -- 補完ウィンドウ表示
+					["<C-e>"] = cmp.mapping.abort(), -- 補完中止
+					["<Tab>"] = cmp.mapping.confirm({ select = true }), -- 補完確定
+					-- ["<Tab>"] = cmp.mapping(function(fallback)
+					-- 	if cmp.visible() then
+					-- 		cmp.select_next_item()
+					-- 	elseif luasnip.expand_or_jumpable() then
+					-- 		luasnip.expand_or_jump()
+					-- 	else
+					-- 		fallback()
+					-- 	end
+					-- end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					-- <C-p> と <C-n> のマッピングをオーバーライド
+					["<C-p>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							-- 補完メニュー非表示時はグローバルマッピング (<Up>) を使用
+							fallback()
+						end
+					end, { "i" }), -- インサートモードのみで適用
+					["<C-n>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							-- 補完メニュー非表示時はグローバルマッピング (<Down>) を使用
+							fallback()
+						end
+					end, { "i" }), -- インサートモードのみで適用
+				}),
+			})
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline({
-                    ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "path" },
-                }, {
-                    { name = "cmdline" },
-                }),
-            })
-        end,
-    },
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline({
+					["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
 }
--- private_dot_config/nvim/lua/plugins/nvim-tree.lua
+++ private_dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,30 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    config = true,
-    keys = {
-        { mode = "n", "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "NvimTreeをトグルする" },
-        { mode = "n", "<C-m>", "<cmd>NvimTreeFocus<CR>", desc = "NvimTreeにフォーカス" },
-        { mode = "n", "<leader>ee", "<cmd>NvimTreeFindFileToggle<CR>", desc = "NvimTreeでファイルを見つける" },
-    }
+	"nvim-tree/nvim-tree.lua",
+	config = true,
+	opts = {
+		update_focused_file = {
+			enable = true,
+			update_cwd = false,
+			update_root = false, -- ルートディレクトリは変更しない
+			ignore_list = { ".git", "node_modules", ".cache" }, -- 無視するディレクトリ
+		},
+		-- その他の便利な設定
+		view = {
+			width = 30,
+			side = "left",
+		},
+		filters = {
+			dotfiles = false, -- 隠しファイルを表示
+		},
+	},
+	keys = {
+		{ mode = "n", "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "NvimTreeをトグルする" },
+		{ mode = "n", "<C-m>", "<cmd>NvimTreeFocus<CR>", desc = "NvimTreeにフォーカス" },
+		{
+			mode = "n",
+			"<leader>ee",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			desc = "NvimTreeでファイルを見つける",
+		},
+	},
 }
